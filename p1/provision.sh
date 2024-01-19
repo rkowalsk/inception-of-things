43bbbe997e2237bd8fe28a9986bf2a331d02eb23
Romain Kowalski <rkowalsk@z3r4p5.42lyon.fr>
2024-01-19 18:35:41 +0100

    p1 done (kubectl not working in agent but who cares)

--- p1/provision.sh
+++ p1/provision.sh
@@ -11,7 +11,14 @@ elif [ "$1" == "SW" ]; then
 	echo "Provisionning Server Worker"
 else
 	echo "S or SW you twat"
+	exit 1
 fi
 
 apt-get update
-curl -sfL https://get.k3s.io | sh -
+apt-get install -y curl
+
+if [ "$1" == "S" ]; then
+	curl -sfL https://get.k3s.io | K3S_TOKEN=amogus sh -s - server --flannel-iface eth1
+elif [ "$1" == "SW" ]; then
+	curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=amogus sh -s - --flannel-iface eth1
+fi
