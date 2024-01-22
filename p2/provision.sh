ba2a5e2793490674c197e1d530cbf04a9f4d72d2
Damien Puccioni <dpuccion@z3r3p2.42lyon.fr>
2024-01-22 17:01:32 +0100

    services, apps and ingress (not complete) created, app ready only when one is created

--- p2/provision.sh
+++ p2/provision.sh
@@ -4,4 +4,12 @@ echo "Provisioning Server"
 
 apt-get update
 apt-get install -y curl
-curl -sfL https://get.k3s.io | K3S_TOKEN=amogus sh -s - server --flannel-iface eth1
+curl -sfL https://get.k3s.io | sh -s - server --flannel-iface eth1
+sudo chmod 644 /etc/rancher/k3s/k3s.yaml
+kubectl apply -f "/vagrant/deployment/app1.yaml"
+#kubectl apply -f "/vagrant/deployment/app2.yaml"
+#kubectl apply -f "/vagrant/deployment/app3.yaml"
+#kubectl create service clusterip app-one --tcp=80:80
+#kubectl create service clusterip app-two --tcp=80:80
+#kubectl create service clusterip app-three --tcp=80:80
+#kubectl apply -f "/vagrant/deployment/ingress.yaml"
