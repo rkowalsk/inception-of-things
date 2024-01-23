0ac49c6fa58adb40416cb96ba38d628254abe3f8
Romain Kowalski <rkowalsk@z3r3p3.42lyon.fr>
2024-01-23 14:04:34 +0100

    all services and deployments work

--- p2/provision.sh
+++ p2/provision.sh
@@ -5,10 +5,8 @@ echo "Provisioning Server"
 apt-get update
 apt-get install -y curl
 curl -sfL https://get.k3s.io | sh -s - server --flannel-iface eth1
-sudo chmod 644 /etc/rancher/k3s/k3s.yaml
-kubectl apply -f "/vagrant/deployment/app1.yaml"
-#kubectl apply -f "/vagrant/deployment/app2.yaml"
-#kubectl apply -f "/vagrant/deployment/app3.yaml"
+chmod 644 /etc/rancher/k3s/k3s.yaml
+kubectl apply -f "/vagrant/deployment/"
 #kubectl create service clusterip app-one --tcp=80:80
 #kubectl create service clusterip app-two --tcp=80:80
 #kubectl create service clusterip app-three --tcp=80:80
