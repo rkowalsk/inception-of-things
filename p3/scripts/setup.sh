3e68a3b15d364e415e1cb30f93bca4715184aa1a
rkowalsk <rkowalsk@debian>
2024-01-26 15:29:52 +0100

    argocd app almost working (close imo)

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-k3d cluster create p3 --port 8080:80@loadbalancer --port 8443:443@loadbalancer
+k3d cluster create p3 --port 80:80@loadbalancer --port 443:443@loadbalancer
 kubectl create namespace argocd
+kubectl create namespace dev
 kubectl create -n argocd -f ../confs/install.yaml
 kubectl apply -f ../confs/ingress.yaml -n argocd
 echo "The password for 'admin' account is :"
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo
-kubectl create namespace dev
