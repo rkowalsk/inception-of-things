19e036b7aa351392ebdbb52122921981e7d5225d
rkowalsk <rkowalsk@debian>
2024-02-01 14:55:02 +0100

    gitlab working

--- bonus/scripts/setup.sh
+++ bonus/scripts/setup.sh
@@ -9,6 +9,11 @@ kubectl create namespace gitlab
 kubectl create -n argocd -f ../confs/install.yaml
 kubectl apply -f ../confs/ingress_argocd.yaml -n argocd
 kubectl apply -f ../confs/ingress_dev.yaml -n dev
-helm install -n gitlab gitlab gitlab/gitlab --set certmanager-issuer.email=me@example.com \
---set global.hosts.domain=localhost \
---set global.hosts.externalIP=127.0.0.1
+kubectl apply -f ../confs/ingress_gitlab.yaml -n gitlab
+helm install -n gitlab gitlab gitlab/gitlab \
+--set global.hosts.https="false" \
+--set global.ingress.configureCertmanager=false
+# --set global.hosts.domain=gitlab.example.com \
+# --set global.hosts.externalIP=127.0.0.1 \
+# --set nginx-ingress.enabled=false \
+# --set certmanager.install=false \
